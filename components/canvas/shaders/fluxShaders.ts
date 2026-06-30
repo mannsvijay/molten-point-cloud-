848071f6e5d59d46f4e29fba8f20557317eb94f6
mannsvijay <mnvj69@gmail.com>
2026-06-30 23:13:06 +0530

    fix: update permute function to accept vec4 instead of vec3

--- components/canvas/shaders/fluxShaders.ts
+++ components/canvas/shaders/fluxShaders.ts
@@ -19,7 +19,7 @@ vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
 vec4 mod289(vec4 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
 vec4 mod7(vec4 x) { return x - floor(x * (1.0 / 7.0)) * 7.0; }
 
-vec3 permute(vec3 x) { return mod289(((x * 34.0) + 1.0) * x); }
+vec4 permute(vec4 x) { return mod289(((x * 34.0) + 1.0) * x); }
 
 float snoise(vec3 v) {
   const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
